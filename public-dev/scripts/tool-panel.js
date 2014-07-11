f4004d37980b926be46876f8091c8e42f05ecd56
Andrey Elenskiy <andreye@wix.com>
2014-07-11 15:45:52 -0700

    Adds comments explaining behaviour

--- public-dev/scripts/tool-panel.js
+++ public-dev/scripts/tool-panel.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
+  // open/close tab for following tools
   $('#color-tool, #drag-tool').mousedown(function () {
     var element = $(this);
     $('.tab-open').not(element.parent()).removeClass('tab-open');
     element.parent().toggleClass('tab-open');
   });
 
+  // close tab if clicked anywhere outside it
   $(document).mousedown(function (e) {
     var activeTool = $('.tab-open');
 
@@ -15,6 +17,7 @@ $(document).ready(function() {
     }
   });
 
+  // make following tools active on click
   $('#color-tool, #eraser-tool, #drag-tool, #help-tool').click(function () {
     $('.active').toggleClass('active');
     $(this).toggleClass('active');
